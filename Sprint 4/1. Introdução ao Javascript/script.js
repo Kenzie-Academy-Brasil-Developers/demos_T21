214041248f0dae02eba7faeeb9aa1212c8057214
alexandersilvadev <104785550+alexandersilvadev@users.noreply.github.com>
2023-10-16 11:12:04 -0300

    Add files via upload

---
+++ Sprint 4/1. Introdução ao Javascript/script.js
@@ -0,0 +1,68 @@
+console.log("Olá T21")
+
+// Variáveis
+
+// let  (pode ser reatrubuido o valor)
+
+//  - Declaração de variável
+let nome = "Alex"
+console.log(nome)
+
+// - Reatribuir o valor de uma variável
+
+nome = "Thiago"
+console.log(nome)
+
+// Operação matemática 
+
+let numero1 = 19
+let numero2 = 5
+
+let total = numero1 + numero2
+// let total = 19 + 5
+console.log(total)
+
+// console.log(num1 + num2)
+
+// const (NÃO pode ser reatrubuido o valor)
+
+const cpf = 12345678900
+
+// cpf = 11111111111
+
+console.log(cpf)
+
+// var (NÃO UTILIZAMOS MAIS)
+
+// var idade = 19
+// console.log(idade)
+
+// idade = 20
+// console.log(idade)
+
+// Tipos de Variáveis
+// 1 - Inteiro (number)
+
+let idade = 19
+let numero = 100000
+
+// 2 - Float (number )
+
+let peso = 85.5
+
+// 3 - String (string)
+
+let sobrenome = "Sakaki Silva"
+let texto = "Olá turma 21. Bem vindos a nossa demo de JS."
+
+// 4 - Boleano (boolean)
+
+let verdadeiro = true
+console.log(verdadeiro)
+let falso = false
+
+
+// console.log(sobrenome)
+
+
+// let podeEntrar = true
