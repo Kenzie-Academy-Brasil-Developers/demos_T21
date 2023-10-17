c4a985e7abca500da6352be98f772d627f7347d6
thiagoKenzie <102174562+thiagoKenzie@users.noreply.github.com>
2023-10-17 11:05:06 -0300

    Add files via upload

---
+++ Sprint 4/1. Introdução ao Javascript - noite/script.js
@@ -0,0 +1,45 @@
+// console.log('Testando');
+/* console.error('Testando'); */
+
+//VARIAVEIS
+let palavra1 = "Kenzie";
+const palavra2 = "Academy"; 
+
+let frase = palavra1 + palavra2;
+
+console.log(frase);
+console.log(palavra1);
+console.log(palavra2);
+
+palavra1 = "Escola";
+console.log(palavra1);
+
+palavra2 = "Escola";
+
+
+//TIPOS DE VALORES
+let numero = 12; //number
+let texto = "Kenzie Academy Brasil"; //string
+let boleano = true; //boolean
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof boleano);
+
+
+//OPERAÇÕES MATEMATICAS
+const a = 3;
+const b = 12;
+
+const soma = a + b;          //3 + 12 = 15
+const subtracao = b - a;     //12 - 3 = 9
+const multiplicacao = b * a; //12 * 3 = 36
+const divisao = b / a;       //12 / 3 = 4
+
+console.log(soma, subtracao, multiplicacao, divisao);
+
+const potencia = a**2;       //3² = 9
+console.log(potencia);
+
+
+// let temSocorro = false;
