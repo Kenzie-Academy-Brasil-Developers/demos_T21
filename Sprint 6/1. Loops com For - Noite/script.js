724bb436aa25ac4f54ddd58fcf2669bb5617d508
alexandersilvadev <104785550+alexandersilvadev@users.noreply.github.com>
2023-10-31 09:00:25 -0300

    Add files via upload

---
+++ Sprint 6/1. Loops com For - Noite/script.js
@@ -0,0 +1,65 @@
+// Sintaxe do for
+
+// Inicialização
+// Condição
+// Incremento/decremento/interação
+
+// for (inicialização; condição; incremento) {
+//     Executar um código/instrução
+// }
+
+// for (let index = 1; index <= 10; index++) {
+//     console.log(index)
+// }
+
+// console.log("Saiu do for")
+
+// let contador = 0
+// contador += 3
+
+// for (let i = 0; i <= 30; i *= 2) {
+//     console.log(i)
+// }
+
+// console.log(nome[0])
+// console.log(nome[1])
+// console.log(nome[2])
+// console.log(nome[3])
+
+// console.log(nome.length)
+// let nome = "Capella";
+
+
+// function contaVogal(qualquerString) {
+//     let qtdeVogais = 0;
+
+//     for (let i = 0; i < qualquerString.length; i++) {
+//         if (
+//             qualquerString[i] === "a" ||
+//             qualquerString[i] === "e" ||
+//             qualquerString[i] === "i" ||
+//             qualquerString[i] === "o" ||
+//             qualquerString[i] === "u"
+//         ) {
+//             qtdeVogais++;
+//         }
+//     }
+
+//     return qtdeVogais
+
+// }
+
+// let resultado = contaVogal(nome)
+
+// console.log(resultado)
+
+// for (let i = 1; i <= 10; i++) {
+//         console.log(i)
+//     }
+        //  0 1 2 3
+// let nome = "alexander"
+// //              3             4 é maior que 0? sim  
+// for (let i = nome.length - 1; i >= 0; i--) {
+//     //          nome[0]
+//     console.log(nome[i])
+// }
