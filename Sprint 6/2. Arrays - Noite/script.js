2c2c83b180aba365f8ad0940b898e035f4e8a43e
alexandersilvadev <104785550+alexandersilvadev@users.noreply.github.com>
2023-11-02 08:15:07 -0300

    Add files via upload

---
+++ Sprint 6/2. Arrays - Noite/script.js
@@ -0,0 +1,107 @@
+// Arrays
+
+// let variavel = "Valor da variavel"
+
+// console.log(variavel[0])
+
+// let meuArray = ["Sakaki"] 
+
+// console.log(meuArray)
+
+
+// push sempre adiciona elementos no final do array
+
+// meuArray.push("Alex")
+// meuArray.push("Silva")
+
+// console.log(meuArray)
+
+// unshift sempre adiciona elementos no início do array
+
+// meuArray.unshift("Silva")
+
+// console.log(meuArray)
+
+// pop sempre remove o último elemento do array
+
+// meuArray.pop()
+
+// console.log(meuArray)
+
+// shift sempre remove o primeiro elemento do array
+
+// let itemRemovido = meuArray.shift()
+
+// console.log(itemRemovido)
+// console.log(meuArray)
+
+
+// const novoArray = ["Alex"]
+
+// novoArray.push(10)
+
+// console.log(novoArray)
+
+// let array = ["Vitor", "Denes", "Maycon"]
+
+// for (let i = 0; i < array.length; i++) {
+//     if ("Denes" === array[i]) {
+//         console.log(`O Denes está no indice ${i}`)
+//     }
+// }
+
+
+// function parEImpar (num) {
+//     let numerosPares = []
+//     let numerosImpares = []
+
+//     for (let i = 1; i < num; i++) {
+    
+//         if (i % 2 == 0) {
+//             numerosPares.push(i)
+//         } else {
+//             numerosImpares.push(i)
+//         }
+//     }
+
+//     return numerosPares
+// }
+
+// let retornoDaFuncao = parEImpar(8)
+
+// retornoDaFuncao.push("Par")
+
+// console.log(retornoDaFuncao)
+
+//               0        1        2          3      - Indices
+// let alunos = ["Vitor", "Denes", "Maycon", "Fernando"]
+// // console.log(alunos.length)
+
+// let alunosInversos = []
+
+// for(let i = alunos.length - 1; i >= 0; i--) {
+
+//     let atual = alunos[i]
+//     // O valor da variavel vai ser "fernando"
+
+//     alunosInversos.push(atual)
+   
+// }
+
+// console.log(alunos)
+// console.log(alunosInversos)
+
+
+// let nome = "Alex"
+
+// let nomeNoArray = []
+
+
+// for (let i = 0; i < nome.length; i++) {
+//     nomeNoArray.push(nome[i])
+// }
+
+// console.log(nomeNoArray)
+
+// nomeNoArray.pop()
+// console.log(nomeNoArray)
